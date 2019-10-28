c7a7accd117d4c455592e37c1efaaf40f53c164d
Joonas Harjukallio <harjukallio@gmail.com>
2019-10-28 09:46:45 +0200

    Initial commit

---
+++ src/main/kotlin/com/example/exposeddaodslprogrammatictransaction/ExposedDaoDslProgrammaticTransactionApplication.kt
@@ -0,0 +1,11 @@
+package com.example.exposeddaodslprogrammatictransaction
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExposedDaoDslProgrammaticTransactionApplication
+
+fun main(args: Array<String>) {
+	runApplication<ExposedDaoDslProgrammaticTransactionApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/exposeddaodslprogrammatictransaction/ExposedDaoDslProgrammaticTransactionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.exposeddaodslprogrammatictransaction
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExposedDaoDslProgrammaticTransactionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
